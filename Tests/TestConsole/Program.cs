65ddf55b8022a53f4880807cfec928e242dcdd60
Infarh <shmachilin@yandex.ru>
2020-04-20 20:39:44 +0300

    Пример наследования и полиморфизма

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -18,27 +18,44 @@ namespace TestConsole
 
             //Console.WriteLine(player1.Name);
 
-            Vector2D v1 = new Vector2D(5, 7);
-            Vector2D v2 = new Vector2D(-7, 2);
+            //Vector2D v1 = new Vector2D(5, 7);
+            //Vector2D v2 = new Vector2D(-7, 2);
 
-            Vector2D v3 = v1 + v2;
+            //Vector2D v3 = v1 + v2;
 
-            Vector2D v4 = v3 + 3.14159265358979;
+            //Vector2D v4 = v3 + 3.14159265358979;
 
-            CultureInfo ru = new CultureInfo("ru-ru");
-            CultureInfo en_us = new CultureInfo("en-us");
-            CultureInfo invariant = CultureInfo.InvariantCulture;
-            CultureInfo current = CultureInfo.CurrentCulture;
-            CultureInfo current_ui = CultureInfo.CurrentUICulture;
+            //CultureInfo ru = new CultureInfo("ru-ru");
+            //CultureInfo en_us = new CultureInfo("en-us");
+            //CultureInfo invariant = CultureInfo.InvariantCulture;
+            //CultureInfo current = CultureInfo.CurrentCulture;
+            //CultureInfo current_ui = CultureInfo.CurrentUICulture;
 
-            double pi = double.Parse("3.1415", invariant); //Convert.ToDouble("3,1415");
-            //int j = Convert.ToInt32(3.14);
-            int i = (int)pi;
+            //double pi = double.Parse("3.1415", invariant); //Convert.ToDouble("3,1415");
+            ////int j = Convert.ToInt32(3.14);
+            //int i = (int)pi;
 
-            Console.WriteLine(pi);
-            
+            //Console.WriteLine(pi);
 
-            double length = v4;
+            //double length = v4;
+
+            Printer printer = new Printer();
+            PrefixPrinter prefix_printer = new PrefixPrinter();
+            prefix_printer.Prefix = "!!!!!!!-------!!!!!!!";
+
+            prefix_printer.Print("QWE");
+
+            printer.Print("Hello World!");
+            prefix_printer.PrintData(3.14);
+
+            printer.Print("123");
+
+            printer = prefix_printer;
+
+            Printer printer1 = new PrefixPrinter();
+
+            printer.Print("345");
+            printer1.Print("678");
 
             Console.ReadLine();
         }
