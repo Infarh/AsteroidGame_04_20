67f73f6210a3e23be503476dd3bfddbeb0a0c809
Infarh <shmachilin@yandex.ru>
2020-04-23 19:54:43 +0300

    Логгер для системы трассировки событий

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using TestConsole.Loggers;
+using System.Diagnostics;
 
 namespace TestConsole
 {
@@ -10,7 +11,11 @@ namespace TestConsole
         {
             //Logger log = new TextFileLogger("text.log");
             //Logger log = new ConsoleLogger();
-            Logger log = new DebugOutputLogger();
+            //Logger log = new DebugOutputLogger();
+            Logger log = new TraceLogger();
+
+            Trace.Listeners.Add(new TextWriterTraceListener("logger.log"));
+            Trace.Listeners.Add(new XmlWriterTraceListener("logger.log.xml"));
 
             log.LogInformation("Message1");
             log.LogWarning("Info message");
@@ -18,7 +23,7 @@ namespace TestConsole
 
             log.Flush();
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
