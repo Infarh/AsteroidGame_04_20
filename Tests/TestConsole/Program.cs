9d73407b024307ce8417289677357e28fb7a0f0e
Infarh <shmachilin@yandex.ru>
2020-04-23 19:32:21 +0300

    Описание абстрактного калсса Logger

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using TestConsole.Loggers;
 
 namespace TestConsole
 {
@@ -7,7 +8,11 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-           
+            //Logger log = new Logger();
+
+            //log.LogInformation("Message1");
+            //log.LogWarning("Info message");
+            //log.LogError("Error message");
 
             Console.ReadLine();
         }
