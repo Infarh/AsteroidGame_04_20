4cba5f70680448138ff7003824294999511881be
Infarh <shmachilin@yandex.ru>
2020-04-30 19:31:46 +0300

    Класс-хранилище

---
+++ Tests/TestConsole/Storage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public abstract class Storage<TItem>
+    {
+        private readonly List<TItem> _Items = new List<TItem>();
+
+        public int Count => _Items.Count;
+
+        public TItem this[int index]
+        {
+            get
+            {
+                return _Items[index];
+            }
+            set
+            {
+                if (_Items.Contains(value)) return;
+                _Items[index] = value;
+            }
+        }
+
+        public void Add(TItem item)
+        {
+            if(_Items.Contains(item)) return;
+            _Items.Add(item);
+        }
+
+        public bool Remove(TItem item)
+        {
+            return _Items.Remove(item);
+        }
+
+        public bool IsContains(TItem item)
+        {
+            return _Items.Contains(item);
+        }
+
+        public void Clear()
+        {
+            _Items.Clear();
+        }
+    }
+}
