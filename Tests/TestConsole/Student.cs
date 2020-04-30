629d46ecd143c4f519ce57f673b72e364c94c483
Infarh <shmachilin@yandex.ru>
2020-04-30 19:24:03 +0300

    Доработка студента...

--- Tests/TestConsole/Student.cs
+++ Tests/TestConsole/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    internal class Student
+    internal class Student : IComparable<Student>, IEquatable<Student>, IEquatable<string>
     {
         public int Id { get; set; }
 
@@ -17,5 +17,49 @@ namespace TestConsole
         public string Patronimyc { get; set; }
 
         public int GroupId { get; set; }
+
+        public List<int> Ratings { get; set; } = new List<int>();
+
+        public double AverageRating
+        {
+            get
+            {
+                var result = 0d;
+                foreach (var rating in Ratings)
+                    result += rating;
+                return result / Ratings.Count;
+            }
+        }
+
+        public int CompareTo(Student other)
+        {
+            var current_average_rating = AverageRating;
+            var other_average_rating = other.AverageRating;
+
+            if (Math.Abs(current_average_rating - other_average_rating) < 0.001)
+                return 0;
+            if (current_average_rating > other_average_rating)
+                return +1;
+            else
+                return -1;
+        }
+
+        public bool Equals(Student other)
+        {
+            if (other == null) return false;
+
+            //if (Name == other.Name && Surname == other.Surname && other.Patronimyc == Patronimyc)
+            //    return true;
+            //return false;
+
+            return Name == other.Name && Surname == other.Surname && other.Patronimyc == Patronimyc;
+        }
+
+        public bool Equals(string other)
+        {
+            if (other is null) return false;
+
+            return Name == other || Surname == other || Patronimyc == other;
+        }
     }
 }
