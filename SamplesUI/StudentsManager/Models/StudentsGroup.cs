85c2e1ebf4b465961b775c96157103a48aab3aca
Infarh <shmachilin@yandex.ru>
2020-05-11 20:43:21 +0300

    Визуализация списка груп

--- SamplesUI/StudentsManager/Models/StudentsGroup.cs
+++ SamplesUI/StudentsManager/Models/StudentsGroup.cs
@@ -13,5 +13,7 @@ namespace StudentsManager.Models
         public string Name { get; set; }
 
         public List<Student> Students { get; set; } = new List<Student>();
+
+        public override string ToString() => $"[{Id}]{Name}";
     }
 }
