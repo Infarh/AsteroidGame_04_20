86e0dc3986df85ffab25b5b50a35430fe1b54304
Infarh <shmachilin@yandex.ru>
2020-05-14 21:17:45 +0300

    Модели (сущности) для описания БД

---
+++ DataBase/StudentsDatabaseTest/Data/Entityes/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDatabaseTest.Data.Entityes
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
---
+++ DataBase/StudentsDatabaseTest/Data/Entityes/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDatabaseTest.Data.Entityes
+{
+    public class Student
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public StudentsGroup Group { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
